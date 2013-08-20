91bd625bade85249a9bb1c80b2d7338dc032b140
Stephen Braitsch <stephen@quietless.com>
2013-08-20 09:46:46 -0700

    osx project init

---
+++ src/VideoTriptych.cpp
@@ -0,0 +1,62 @@
+#include "VideoTriptych.h"
+
+//--------------------------------------------------------------
+void VideoTriptych::setup()
+{
+    ofSetWindowPosition(0, 0);
+}
+
+//--------------------------------------------------------------
+void VideoTriptych::update(){
+
+}
+
+//--------------------------------------------------------------
+void VideoTriptych::draw(){
+
+}
+
+//--------------------------------------------------------------
+void VideoTriptych::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void VideoTriptych::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void VideoTriptych::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void VideoTriptych::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void VideoTriptych::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void VideoTriptych::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void VideoTriptych::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void VideoTriptych::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void VideoTriptych::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "ofMain.h"
+#include "VideoTriptych.h"
+
+//========================================================================
+int main( ){
+	ofSetupOpenGL(1024,768,OF_WINDOW);
+	ofRunApp(new VideoTriptych());
+
+}
